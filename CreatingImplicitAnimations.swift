6538792d0be3b0357c1adf234451075f0ba0e22f
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2023-09-30 23:48:43 +0200

    Add files via upload

---
+++ CreatingImplicitAnimations.swift
@@ -0,0 +1,29 @@
+//
+//  CreatingImplicitAnimations.swift
+//  SwiftUIProject6
+//
+//  Created by Marko Zivanovic on 30.9.23..
+//
+
+import SwiftUI
+
+struct CreatingImplicitAnimations: View {
+    
+    @State private var animationAmonut = 1.0
+    
+    var body: some View {
+        Button("Tap me") {
+            // MARK: Do something
+            animationAmonut += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmonut)
+    }
+}
+
+#Preview {
+    CreatingImplicitAnimations()
+}
