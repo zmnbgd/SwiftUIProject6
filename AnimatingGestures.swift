0f6f21fe5524a22f9fb85d9ce0e85c4c7f3e1694
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2023-10-07 23:43:55 +0200

    Add files via upload

--- AnimatingGestures.swift
+++ AnimatingGestures.swift
@@ -15,6 +15,15 @@ struct AnimatingGestures: View {
         LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
             .frame(width: 300, height: 200)
             .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    .onEnded { _ in
+                        withAnimation {
+                            dragAmount = .zero }
+                        }
+            )
     }
 }
 
