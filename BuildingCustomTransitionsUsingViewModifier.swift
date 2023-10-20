104ffe2ed9bda1738ae95b5c8fae19446ac2fe98
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2023-10-20 21:59:40 +0200

    Add files via upload

---
+++ BuildingCustomTransitionsUsingViewModifier.swift
@@ -0,0 +1,57 @@
+//
+//  BuildingCustomTransitionsUsingViewModifier.swift
+//  SwiftUIProject6
+//
+//  Created by Marko Zivanovic on 20.10.23..
+//
+
+import SwiftUI
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0.0, anchor: .topLeading))
+    }
+}
+
+struct BuildingCustomTransitionsUsingViewModifier: View {
+    
+    @State private var isShowingRed = false
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+                .transition(.pivot)
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
+    }
+}
+
+#Preview {
+    BuildingCustomTransitionsUsingViewModifier()
+}
